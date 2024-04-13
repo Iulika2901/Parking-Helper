d8411ca0a0a1d242b12157b05b34fa5f27b1d7cb
Iulika2901 <84437252+Iulika2901@users.noreply.github.com>
2024-04-13 21:23:22 +0300

    Update source.js

--- source.js
+++ source.js
@@ -1,12 +1,10 @@
-console.log("Hello! This is cosole!");
-console.warn("console");
 function showName() {
     var vehicleName = document.getElementById("name").value;
-    var size = document.getElementById("size").value;
+    var size = document.getElementById("size").value; 
 
-    size.trim(); //no extra spaces
-    size.toLowerCase();
-   
+    site = size.trim(); //no extra spaces
+    size=size.toLowerCase();
+    
     var message = "Welcome back! We found a parking spot for your " + size + " "+ vehicleName +  ".";
     var outputElement = document.getElementById("outputName");
     outputElement.innerText = message;
@@ -16,7 +14,20 @@ function showName() {
    var size_nr=size.search("mx");                //size
    var lenght=0; var width=0; var i=0;
  var words=size.split("xm");
- lenght=words[0];
+ lenght=words[0]; 
  width=words[1];
 console.log(lenght, width);
+}
+
+function showParking()
+{
+parking=["Parking P-ta Cipariu","Parking Primaverii 8", "Parking Baisoara","Parking Floresti F.Nr"]
+var outputElement = document.getElementById("outputName");
+while(parking.lenght!=0)
+{
+  var name=parking.pop();  
+  var message =  name + ": ";
+  outputElement.innerText += message; 
+  outputElement.classList.add("h2-font");
+}
 }
